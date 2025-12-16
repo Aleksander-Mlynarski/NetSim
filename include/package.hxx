1c55fd19e7cc6be878b4d8780e7c753e681e1d09
Mateusz Łaś <mateuszlas8@gmail.com>
2025-12-16 13:50:00 +0100

    Added ElementID to types.hxx and structure of class Package to file package.hxx

--- include/package.hxx
+++ include/package.hxx
@@ -1,5 +1,12 @@
 #include "types.hxx"
 
 class Package {
-    
+  ElementID id_;
+  public:
+    Package() = default;
+    Package(ElementID id) : id_(id) {}
+    Package(const Package &) = default;
+    Package(Package &&) = default;
+    ElementID get_id() const {return id_;}
+    ~Package() = default;
 };
--- include/types.hxx
+++ include/types.hxx
@@ -1,2 +1,3 @@
 using size_type = std::size_t;
 using const_iterator = std::list<Package>::const_iterator;
+using ElementID = int;
