474f16c2035bcb74e14c63c8a822fab758dcb1ff
Alek <alek.mlynarski05@gmail.com>
2025-12-30 14:57:42 +0100

    Add unit tests and mocks, update CMake and headers

--- include/types.hxx
+++ include/types.hxx
@@ -1,3 +1,11 @@
+#ifndef TYPES_HXX
+#define TYPES_HXX
+
 using size_type = std::size_t;
 using const_iterator = std::list<Package>::const_iterator;
 using ElementID = int;
+using TimeOffset = int;
+using Time = int;
+using ProbabilityGenerator = double;
+
+#endif // TYPES_HXX
