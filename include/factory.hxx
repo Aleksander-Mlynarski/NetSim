7b435de6dc624ea1b96811a55fd669634cc35163
Mateusz Łaś <mateuszlas8@gmail.com>
2026-01-19 13:39:23 +0100

    Added implementation of methods has_reachable_storehouse and is_consistent, added a declaration of methon has_reachable_storehouse to header file

--- include/factory.hxx
+++ include/factory.hxx
@@ -62,6 +62,7 @@ private:
     container_t collection_;
 };
 
+enum class NodeColor { UNVISITED, VISITED, VERIFIED };
 
 class Factory{
     NodeCollection<Ramp> ramps_;
@@ -69,6 +70,7 @@ class Factory{
     NodeCollection<Storehouse> storehouses_;
     void remove_receiver(NodeCollection<Worker>& workers, ElementID id);
     void remove_receiver(NodeCollection<Storehouse>& storehouses, ElementID id);
+    bool has_reachable_storehouse(const PackageSender* sender, std::map<const PackageSender*, NodeColor>& node_colors);
 public:
   Factory() = default;
   ~Factory() = default;
